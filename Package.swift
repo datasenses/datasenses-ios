33594db4dbae21d7b7f1cbc0d6f26e58e2634d7c
Hoang Ho <hohuyhoang01011990@gmail.com>
2024-08-07 17:00:10 +0700

    Add: Version 0.3.2 with Swift package manager

--- Package.swift
+++ Package.swift
@@ -13,15 +13,18 @@ let package = Package(
             name: "datasenses-sdk",
             targets: ["datasenses-sdk"]),
     ],
+    dependencies: [
+        .package(name: "datasenses-ios", url: "https://github.com/datasenses/datasenses-ios.git", .branch("main"))
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "datasenses-sdk",
             url: "https://cdn.datasenses.io/Datasenses_iOS.xcframework.zip",
-	    checksum: "99e9ec8951ee3c4479aee456ca69251bcf9b9b405593277ab46dd8dafbccb377"
+            checksum: "99e9ec8951ee3c4479aee456ca69251bcf9b9b405593277ab46dd8dafbccb377"
         ),
-        
+        .target(
+            name: "datasenses-sdk",
+            dependencies: ["datasenses-ios"]),
     ],
     swiftLanguageVersions: [.v5]
 )
